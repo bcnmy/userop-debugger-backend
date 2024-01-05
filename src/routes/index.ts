5e3643158df18b9c2ec4b6261d5276739811a9fb
iamonuwa <matrix4u2002@gmail.com>
2024-01-05 12:22:53 +0000

    chore(route): remove config route from api versioning

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,8 +6,8 @@ import { validateNetworkId } from '../middlewares/rpc-validation/network-validat
 const router = new Router();
 
 export const registerRoutes = (app: Koa) => {
+    router.post('/config', debugUserOpRequest.middleware);
     router.post('/api/v1/:networkId', validateNetworkId, debugUserOpRequest.middleware);
-    router.post('/api/v1/config', debugUserOpRequest.middleware);
     app.use(router.routes());
     return app;
 }
