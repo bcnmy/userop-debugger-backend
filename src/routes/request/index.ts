a369a19da7de5d459268e4fda20eaf86b639b442
iamonuwa <matrix4u2002@gmail.com>
2024-01-05 13:23:02 +0000

    chore(route): refactor config route

--- src/routes/request/index.ts
+++ src/routes/request/index.ts
@@ -1,6 +1,7 @@
 import debugUserOp from '../../controller/DebugUserOpController';
 import { jsonRPCRouter } from '../../config/router';
 import { supportedNetworks } from '../../config';
+import { networkConfig } from '../../config/network';
 
 jsonRPCRouter.method("eth_debugUserOperation", async (ctx: any) => {
     const params = ctx.request.body.params;
@@ -13,9 +14,11 @@ jsonRPCRouter.method("eth_debugUserOperation", async (ctx: any) => {
 
 // Add other rpc methods here as mentioned above
 jsonRPCRouter.method("eth_config", async (ctx: any) => {
-    ctx.body = {
-        supportedNetworks,
-    };
+    const data = supportedNetworks.map((network) => ({
+        label: networkConfig[network].nativeSymbol,
+        value: network,
+    }));
+    ctx.body = data;
     ctx.status = 200;
 });
 
